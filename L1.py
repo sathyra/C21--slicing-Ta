05d543bea322cf3a26af6f54d795a665c1217985
sathyra <105861021+sathyra@users.noreply.github.com>
2022-06-14 14:51:02 +0530

    Add files via upload

---
+++ L1.py
@@ -0,0 +1,13 @@
+import numpy as np
+from tkinter import *
+root=Tk()
+root.geometry("600x400")
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+#Defining the slicing function
+def sl_ar():
+        i = int(input1.get())
+        j = int(input2.get())
+        #Write a code to add the slcing operation
+        label.config(text=newarr1)
+ 
